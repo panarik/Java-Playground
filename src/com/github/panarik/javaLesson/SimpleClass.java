683774c3bb8ded6853d31f087bdf682d99c14a1c
panarik <panarik@yandex.ru>
2021-02-16 21:40:13 +0300

    init 4

--- src/com/github/panarik/javaLesson/SimpleClass.java
+++ src/com/github/panarik/javaLesson/SimpleClass.java
@@ -1,10 +1,10 @@
 package com.github.panarik.javaLesson;
 
-public class FirstClass {
+public class SimpleClass {
 
     public static void main(String[] args) {
 
-        System.out.println("Test");
+        System.out.println("Test it");
         //test2
     }
 
