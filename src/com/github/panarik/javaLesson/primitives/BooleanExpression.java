3a3a783a67362ac89384f64c6b19610b3dbbc3d0
panarik <panarik@yandex.ru>
2021-02-17 00:44:25 +0300

    [primitive] BooleanExpression - step 1

---
+++ src/com/github/panarik/javaLesson/primitives/BooleanExpression.java
@@ -0,0 +1,42 @@
+package com.github.panarik.javaLesson.primitives;
+
+public class BooleanExpression {
+
+    public static void main(String[] args) {
+
+        // шаг 1: Перебор объектов с помощью графов в комбинаторных задачах
+        boolean tttt = boolianExpression(true, true, true, true);
+        boolean tttf = boolianExpression(true, true, true, false);
+        boolean ttft;
+        boolean ttff;
+        boolean tftt;
+        boolean tftf;
+        boolean tfft;
+        boolean tfff;
+        boolean fttt;
+        boolean fttf;
+        boolean ftft;
+        boolean ftff;
+        boolean fftt;
+        boolean fftf;
+        boolean ffft;
+        boolean ffff;
+
+
+    }
+
+
+    private static boolean boolianExpression(boolean a, boolean b, boolean c, boolean d) {
+
+        boolean result = false;
+
+        /*
+            != - отрицание
+            && - И
+            || - ИЛИ
+            ^ - исключающее или
+        */
+
+        return (result);
+    }
+}
