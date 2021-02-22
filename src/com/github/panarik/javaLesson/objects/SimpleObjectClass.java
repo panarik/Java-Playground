cd7b1bd128824286572f40b9e373ec08404aff32
panarik <panarik@yandex.ru>
2021-02-22 14:59:23 +0300

    [objects] create simple object

---
+++ src/com/github/panarik/javaLesson/objects/SimpleObjectClass.java
@@ -0,0 +1,21 @@
+package com.github.panarik.javaLesson.objects;
+
+public class SimpleObjectClass {
+
+    public static void main(String[] args) {
+
+        //инициализируем новый объект класса Person
+        Person person = new Person();
+
+    }
+
+
+    //
+    public static class Person {
+
+        String name;
+        int age;
+
+    }
+
+}
