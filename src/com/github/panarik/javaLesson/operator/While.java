70f78f335b7fe45b2e3a16ec3514b731a1e53265
panarik <panarik@yandex.ru>
2021-02-26 19:39:56 +0300

    [operator] add three levels

--- src/com/github/panarik/javaLesson/operator/While.java
+++ src/com/github/panarik/javaLesson/operator/While.java
@@ -10,22 +10,26 @@ public class While {
 
         System.out.println("Ваша задача угадать число.");
 
-        //1-ый уровень
-        int range = 10; //диапазон значений для угадывания в игре
-        int number = (int) (Math.random() //Загаданное число. присваиваем переменной number случайное число от 0 до 1
-                * range);//помноженное на 10, тоесть уже от 0 до 10
-        playLevel(range, number);
-        scanner.close(); //освобождаем ресурсы взятые сканнером
+        //по каждому шагу цикла запускаем метод с очередным уровнем игры:
+        // - загадыванием числа
+        // - проверкой введённого ответа
+        for (int i = 10; i <= 30; i = i + 10) playLevel(i);
+
+        System.out.println("Вы выиграли!!!");
+        scanner.close(); //в конце игры освобождаем ресурсы взятые сканнером
     }
 
-    private static void playLevel(int range, int number) {
+    private static void playLevel(int range) {
+
+        int number = (int) (Math.random() //В начале уровня создаём загаданное число. присваиваем переменной number случайное число от 0 до 1
+                * range);//помноженное на i, тоесть берем из текущего значения в цикле
 
         while (true) { //запускаем цикл while (бесконечный т.к. в параметре всегда TRUE)
             System.out.println("Угадайте число от 0 до " + range);
             int input_number = scanner.nextInt(); //пользователь вводит число в переменную input_number
 
             if (input_number == number) {
-                System.out.println("Вы угадали! Загадано число "+number+" !!!");
+                System.out.println("Вы угадали! Загадано число " + number + " !!!");
                 break; //только после правильного ответа цикл завершается
             } else if (input_number < number & input_number * 2 >= number) {
                 System.out.println("Неправильно. Загаданное число немного больше.");
