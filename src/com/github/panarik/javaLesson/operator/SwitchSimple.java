606f589b5c91d40a928631d229447557e1e4322e
panarik <panarik@yandex.ru>
2021-03-16 19:39:07 +0300

    [operator] add simple switch

---
+++ src/com/github/panarik/javaLesson/operator/SwitchSimple.java
@@ -0,0 +1,24 @@
+package com.github.panarik.javaLesson.operator;
+
+public class SwitchSimple {
+
+    public static void main(String[] args) {
+
+        String recipient = "1";
+
+        switch (recipient) {
+            case "Petya":
+                System.out.println("Box #1");
+                break;
+            case "Vova":
+                System.out.println("Box #2");
+                break;
+            case "Ivan":
+                System.out.println("Box #3");
+                break;
+            default:
+                System.out.println("Nothing");
+        }
+
+    }
+}
