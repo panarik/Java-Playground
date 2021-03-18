74b8d4bb120511cff7de4423df28171e50d6c72a
panarik <panarik@yandex.ru>
2021-03-18 21:24:49 +0300

    [task2] add solution 1

---
+++ src/com/github/panarik/javaLesson/task2/SolutionOne.java
@@ -0,0 +1,29 @@
+package com.github.panarik.javaLesson.task2;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class SolutionOne {
+
+    /*
+     * 1. Задать целочисленный массив, состоящий из элементов 0 и 1.
+     * Например: [ 1, 1, 0, 0, 1, 0, 1, 1, 0, 0 ].
+     * С помощью цикла и условия заменить 0 на 1, 1 на 0;
+     */
+
+    public static void main(String[] args) {
+
+        int arr[] = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
+
+        System.out.println("Массив array: " + Arrays.toString(arr));
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 0) arr[i] = 1;
+            else arr[i] = 0;
+        }
+
+        System.out.println("Массив array после инверсии: " + Arrays.toString(arr));
+
+    }
+
+}
