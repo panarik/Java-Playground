d2c0a2442245a43c4916d40f0b6971e26b9bfaf6
panarik <panarik@yandex.ru>
2021-03-21 17:05:35 +0300

    [work4] task1 - init

---
+++ src/com/github/panarik/javaLesson/work4/task1/Solution.java
@@ -0,0 +1,26 @@
+package com.github.panarik.javaLesson.work4.task1;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Solution {
+
+   /*
+    * 1. Полностью разобраться с кодом, попробовать переписать с нуля, стараясь не подглядывать в методичку.
+    * 2. Переделать проверку победы, чтобы она не была реализована просто набором условий, например, с использованием циклов.
+    */
+
+    private static int[][] field; //игровое поле
+    private static final Random RANDOM = new Random(); //ввод AI случайного поля
+    private static final Scanner SCANNER = new Scanner(System.in); //ввод пользователем данных
+    private static final char DOT_HUMAN = 'X';
+    private static final char DOT_AI = '0';
+
+
+    public static void main(String[] args) {
+
+
+
+    }
+
+}
