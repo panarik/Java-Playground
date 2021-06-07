60d7245b1029b84e87531c2a9522af75b2c44f78
panarik <panarik@yandex.ru>
2021-06-08 00:38:12 +0300

    add http client (part 2)

--- src/main/java/com/github/panarik/javaLesson/net/httpClientSample/Main.java
+++ src/main/java/com/github/panarik/javaLesson/net/httpClientSample/Main.java
@@ -1,11 +1,15 @@
-package com.github.panarik.javaLesson.net.httpclient;
+package com.github.panarik.javaLesson.net.httpClientSample;
 
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.List;
 
 public class Main {
 
@@ -25,8 +29,15 @@ public class Main {
 
         //получаем ответ
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+//        System.out.println(response.body());
 
-        System.out.println(response.body());
+        //парсим JSON
+        ObjectMapper mapper = new ObjectMapper();
+
+        List<Post> posts = mapper.readValue(response.body(), new TypeReference<List<Post>>() {});
+
+        posts.forEach(System.out::println);
+
     }
 
 }
