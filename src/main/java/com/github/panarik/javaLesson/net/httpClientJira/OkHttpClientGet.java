741b378b9a5d97794e9269d4f6299dd5ec782aab
panarik <panarik@yandex.ru>
2021-06-09 23:39:42 +0300

    get Jira issue history

---
+++ src/main/java/com/github/panarik/javaLesson/net/httpClientJira/OkHttpClientGet.java
@@ -0,0 +1,8 @@
+package com.github.panarik.javaLesson.net.httpClientJira;
+
+public interface OkHttpClientGet {
+
+    String URL = "https://panariks.atlassian.net";
+    String ISSUEKEY = "TEST-1";
+
+}
--- src/main/java/com/github/panarik/javaLesson/net/httpClientJira/OkHttpClientJira.java
+++ src/main/java/com/github/panarik/javaLesson/net/httpClientJira/OkHttpClientJira.java
@@ -7,28 +7,50 @@ import okhttp3.Response;
 import java.io.IOException;
 import java.util.Scanner;
 
-public class OkHttpClientJira {
+public class OkHttpClientJira implements OkHttpClientGet {
+
+    private static String authToken;
+    private static final String issueKey = "TEST-1";
 
     public static void main(String[] args) throws IOException {
 
         Scanner scanner = new Scanner(System.in);
         System.out.print("Insert Your Token>>>>>>");
-        String token = scanner.next();
+        authToken = scanner.next();
 
+//        getIssue(issueKey);
+        getIssueHistory(issueKey);
+
+    }
+
+    private static void getIssueHistory(String issueKey) throws IOException {
         OkHttpClient client = new OkHttpClient().newBuilder()
                 .build();
         Request request = new Request.Builder()
-                .url("https://panariks.atlassian.net/rest/api/2/issue/TEST-1")
+                .url("https://panariks.atlassian.net/rest/api/2/issue/"+issueKey+"/changelog?startAt=0&maxResults=100")
                 .method("GET", null)
-                .addHeader("Authorization", "Basic "+token)
+                .addHeader("Authorization", "Basic "+authToken)
                 .build();
         System.out.println(request);
         Response response = client.newCall(request).execute();
 
         System.out.println(response);
         System.out.println(response.body().string());
+    }
 
+    private static void getIssue(String issueKey) throws IOException {
+        OkHttpClient client = new OkHttpClient().newBuilder()
+                .build();
+        Request request = new Request.Builder()
+                .url("https://panariks.atlassian.net/rest/api/2/issue/"+issueKey)
+                .method("GET", null)
+                .addHeader("Authorization", "Basic "+authToken)
+                .build();
+        System.out.println(request);
+        Response response = client.newCall(request).execute();
 
+        System.out.println(response);
+        System.out.println(response.body().string());
     }
 
 }
