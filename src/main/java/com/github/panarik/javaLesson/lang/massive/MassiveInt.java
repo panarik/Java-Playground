7b166687069593e059a864d2eaf88a3ad24d3923
panarik <panarik@yandex.ru>
2021-08-29 19:51:58 +0300

    add int[] sort

--- src/main/java/com/github/panarik/javaLesson/lang/massive/MassiveInt.java
+++ src/main/java/com/github/panarik/javaLesson/lang/massive/MassiveInt.java
@@ -1,5 +1,10 @@
 package com.github.panarik.javaLesson.lang.massive;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+
 public class MassiveInt {
 
 
@@ -12,8 +17,6 @@ public class MassiveInt {
         intMassive[3] = 55;
         intMassive[4] = -200;
 
-
-
         //суммируем
         int sum = 0;
         for (int i = 0; i < intMassive.length; i++) {
@@ -24,9 +27,9 @@ public class MassiveInt {
 
         //ищем значение
         for (int i = 0; i < intMassive.length; i++) {
-            System.out.println("перебираю элемент["+i+"]: = "+intMassive[i]);
+            System.out.println("перебираю элемент[" + i + "]: = " + intMassive[i]);
             if (intMassive[i] == 5) {
-                System.out.println("в элементе №"+i+" Нашел 5!");
+                System.out.println("в элементе №" + i + " Нашел 5!");
                 break;
             }
         }
@@ -35,13 +38,45 @@ public class MassiveInt {
 
         // по условию пропускаем итерации в цикле.
         // тут нати ошибку. Скорее всего в условии поставить вместо номера само значение в текущем элементе
-        for (int i = 0; i< intMassive.length; i++) {
+        for (int i = 0; i < intMassive.length; i++) {
             if (i % 2 != 0) continue;
-            System.out.println("Число "+intMassive[i]+" - четное");
+            System.out.println("Число " + intMassive[i] + " - четное");
         }
 
+        //сортировка руками (по возрастанию)
+        int[] array = {4, 2, 78, 1, 56, 0, 9};
+        for (int i = 1; i < array.length; i++) {
+            // временная позиция в цикле,
+            // которую будем:
+            //      перебирать от текущей к началу,
+            //      и перетаскивать значения от каждой предыдущей позиции к каждой текущей
+            int cash = i;
+            //текущее значение,
+            //которое будем тащить в начало массива,
+            //до тех пор пока будут перед ним значения больше
+            int current = array[i];
 
+            while (cash > 0 && array[cash - 1] > current) {
+                array[cash] = array[cash - 1];
+                cash--;
+            }
+            array[cash] = current;
+        }
+        System.out.println("по возрастанию: "+Arrays.toString(array));
 
+        //аналогично по убыванию
+        int[] arr2 = {1,5,32,0,9,7,20};
+
+        for(int j = 1; j< arr2.length; j++) {
+            int current = arr2[j];
+            int cash = j;
+            while (cash>0 && arr2[cash-1]<current) {
+                arr2[cash] = arr2[cash-1];
+                cash--;
+            }
+            arr2[cash] = current;
+        }
+        System.out.println("по убыванию: "+Arrays.toString(arr2));
     }
 
 
--- src/main/java/com/github/panarik/javaLesson/streams/FileStream.java
+++ src/main/java/com/github/panarik/javaLesson/streams/FileStream.java
@@ -1,14 +1,83 @@
 package com.github.panarik.javaLesson.streams;
 
-import java.io.File;
-import java.io.FilenameFilter;
-import java.io.IOException;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 
 public class FileStream {
 
     public static void main(String[] args) throws IOException {
 
+//        fileEx(); //создание, удаление, перемещение, операции с файлами
+//        fileWrite(); //запись в файл
+        fileRead(); //чтение из файла
+
+
+
+    }
+
+    private static void fileRead() throws IOException {
+        //читаем файл по байтам
+        long startByte = System.currentTimeMillis();
+        try (FileInputStream fis = new FileInputStream("src/main/resources/helloString.txt")) {
+            int x;
+
+            while ((x = fis.read() /* читаем по байтам из файла, складываем значение в виде int*/) > -1 /* пока не дочитаем до конца*/) {
+                System.out.print((char) x);
+            }
+        }
+//        System.out.println("Read on Byte: " + (System.currentTimeMillis() - startByte));
+
+        //читаем чайл с буфером
+        long startBuf = System.currentTimeMillis();
+        byte[] buf = new byte[32];
+        try (FileInputStream fis = new FileInputStream("src/main/resources/helloString.txt")) {
+            int x;
+            while ((x = fis.read(buf) /*читаем по буферам из файла*/) > 0 /* пока буферов не останется*/) {
+//                System.out.print(new String(buf));
+            }
+        }
+//        System.out.println("Read on Buffer: " + (System.currentTimeMillis() - startBuf));
+
+        //читаем с помощью BufferedInputStream
+        long startBufferedStream = System.currentTimeMillis();
+        try (BufferedInputStream bis = new BufferedInputStream(new FileInputStream("src/main/resources/helloString.txt"))) {
+            int x;
+            while ((x = bis.read()) >= 0) {
+//                System.out.print((char) x);
+            }
+        }
+//        System.out.println("Read on BufferedInputStream: " + (System.currentTimeMillis() - startBufferedStream));
+    }
+
+    private static void fileWrite() {
+        //записываем строку в файл
+        byte[] hello = "Hello world!".getBytes(StandardCharsets.UTF_8); //преобразовываем в байты
+        try (FileOutputStream fos = new FileOutputStream("src/main/resources/hello.txt")) { //путь к файлику (с созданием файлика)
+            fos.write(hello);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        //записываем байты в файл в цикле
+        try (FileOutputStream fos = new FileOutputStream("src/main/resources/helloByte.txt")) { //путь к файлику (с созданием файлика)
+            for (int i = 0; i < 100; i++) {
+                fos.write(i);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        //дописываем в существующий файл текст
+        byte[] hello3 = "Hello world!".getBytes(StandardCharsets.UTF_8); //преобразовываем в байты
+        try (FileOutputStream fos = new FileOutputStream("src/main/resources/helloByte.txt", true)) {
+            fos.write(hello3);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void fileEx() throws IOException {
         File file = new File("src/main/resources/test_file.txt");
         //проверяем, существует ли этот файл
         System.out.println("file test_file.txt exist or not? - " + file.exists());
@@ -66,9 +135,7 @@ public class FileStream {
                                         }
                                     }
         );
-        System.out.println("files name startWith jira: "+ Arrays.toString(list));
-
-
+        System.out.println("files name startWith jira: " + Arrays.toString(list));
     }
 
 
