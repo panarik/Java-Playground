4f464efaa52664d717fda6f5211671f23d37c6f6
panarik <panarik@yandex.ru>
2022-03-15 18:00:54 +0300

    added recursive sum.

--- src/main/java/com/github/panarik/javaLesson/lessons/algorithms/recursion/RecursionExapmle.java
+++ src/main/java/com/github/panarik/javaLesson/lessons/algorithms/recursion/RecursionExapmle.java
@@ -3,32 +3,35 @@ package com.github.panarik.javaLesson.lessons.algorithms.recursion;
 public class RecursionExapmle {
 
     public static void main(String[] args) {
-        //recursion();
-        sumFor();
+//        recursionInfinite();
+//        recursionSimple(5); // 5 steps recursion
+        System.out.println(sumRecursion(123));
     }
 
-    private static int sum(int a, int b) {
-        return a+b;
-    }
-
-    // Сложение методом for()
-    private static void sumFor() {
-        int[] array = {2, 4, 6};
-        int sum = 0;
-        for (int i : array) {
-            sum += i;
+    private static void recursionSimple(int i) {
+        if (i <= 0) {
+            System.out.println("Done!");
+            return;
+        } else {
+            System.out.println("Remaining:" + i);
+            i--;
+            recursionSimple(i);
         }
-        System.out.println("result: " + sum);
+    }
+
+    // Сложение методом рекурсии
+    private static int sumRecursion(int digits) {
+        if (digits <= 0) return 0; // Exit from method if it contains nothing.
+        int temp = digits % 10; // Give the last digit.
+        int otherDigits = sumRecursion(digits / 10);
+        int result = temp + otherDigits; // Return one digit from stack plus another from method.
+        return result;
     }
 
     //Бесконечная рекурсия.
-    private static void recursion() {
-        for (int i = 0; i < 10; i++) {
-            if (true) {
-                System.out.println("start new recursion");
-                recursion();
-            }
-        }
+    private static void recursionInfinite() {
+        System.out.println("start new recursion");
+        recursionInfinite();
     }
 
 }
