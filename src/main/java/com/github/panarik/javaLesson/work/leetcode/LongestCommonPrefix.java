c625d636eab06c2f0b85532178351adce27fec3a
panarik <panarik@yandex.ru>
2022-04-02 13:57:42 +0300

    fix some bugs.

--- src/main/java/com/github/panarik/javaLesson/work/leetcode/LongestCommonPrefix.java
+++ src/main/java/com/github/panarik/javaLesson/work/leetcode/LongestCommonPrefix.java
@@ -9,29 +9,49 @@ public class LongestCommonPrefix {
 
     public static void main(String[] args) {
         String[] strs = {"flower", "flow", "flight"};
+        String[] strs1 = {"dog", "racecar", "car"};
+        String[] strs2 = {"dog", "", "car"};
+        String[] strs3 = {"a"};
+        String[] strs4 = {"ab", "a"};
+        String[] strs5 = {"a", "ab"};
+        String[] strs6 = {""};
         System.out.println(new LongestCommonPrefix().longestCommonPrefix(strs));
+        System.out.println(new LongestCommonPrefix().longestCommonPrefix(strs1));
+        System.out.println(new LongestCommonPrefix().longestCommonPrefix(strs2));
+        System.out.println(new LongestCommonPrefix().longestCommonPrefix(strs3));
+        System.out.println(new LongestCommonPrefix().longestCommonPrefix(strs4));
+        System.out.println(new LongestCommonPrefix().longestCommonPrefix(strs5));
+        System.out.println(new LongestCommonPrefix().longestCommonPrefix(strs6));
     }
 
     public String longestCommonPrefix(String[] strs) {
         StringBuilder prefix = new StringBuilder(strs[0]);
         int letterNumber = 0; // Letter in each string.
-        int stringPos = 1; // String position in array. (we already peak first string, and we need check second string)
+        int stringPos = 0; // String position in array.
 
         // Get prefix for each string.
         for (; letterNumber < strs[stringPos].length(); letterNumber++) {
 
             // Do that for all strings.
             for (; stringPos < strs.length; stringPos++) {
-                char ch = strs[stringPos].charAt(letterNumber); // Current letter.
+                if (strs[stringPos].length() == 0) return "";
+                char ch=' '; // Current letter.
+                try {
+                    ch = strs[stringPos].charAt(letterNumber);
+                } catch (StringIndexOutOfBoundsException e) {
+                    prefix.delete(letterNumber, prefix.length());
+                    return prefix.toString();
+                }
+
                 if (ch != prefix.charAt(letterNumber)) { // Match with prefix letter.
-                    prefix.delete(stringPos, prefix.length() - stringPos);
+                    prefix.delete(letterNumber, prefix.length());
                     return prefix.toString();
                 }
             }
-            stringPos = 1;
+            stringPos = 0;
         }
 
-        return "";
+        return prefix.toString();
     }
 
 }
